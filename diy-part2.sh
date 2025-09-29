f5de07446bf56c1d486d5952059d4dac4260ac5e
golldenstj <70574688+golldenstj@users.noreply.github.com>
2025-09-29 13:53:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,23 +19,23 @@
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
-# ÒÆ³ı openwrt feeds ×Ô´øµÄºËĞÄ°ü
+# ç§»é™¤ openwrt feeds è‡ªå¸¦çš„æ ¸å¿ƒåŒ…
 rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
 git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
 
-# ¸üĞÂ golang 1.25 °æ±¾
+# æ›´æ–° golang 1.25 ç‰ˆæœ¬
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
 
-# ĞŞ¸Äfrp°æ±¾Îª¹ÙÍø×îĞÂv0.64.0 https://github.com/fatedier/frp ¸ñÊ½£ºhttps://codeload.github.com/fatedier/frp/tar.gz/v${PKG_VERSION}?
+# ä¿®æ”¹frpç‰ˆæœ¬ä¸ºå®˜ç½‘æœ€æ–°v0.65.0 https://github.com/fatedier/frp æ ¼å¼ï¼šhttps://codeload.github.com/fatedier/frp/tar.gz/v${PKG_VERSION}?
 rm -rf feeds/packages/net/frp
 wget https://github.com/coolsnowwolf/packages/archive/0f7be9fc93d68986c179829d8199824d3183eb60.zip -O OldPackages.zip
 unzip OldPackages.zip
 cp -r packages-0f7be9fc93d68986c179829d8199824d3183eb60/net/frp feeds/packages/net/
 rm -rf OldPackages.zip packages-0f7be9fc93d68986c179829d8199824d3183eb60
-sed -i 's/PKG_VERSION:=0.53.2/PKG_VERSION:=0.64.0/' feeds/packages/net/frp/Makefile
-sed -i 's/PKG_HASH:=ff2a4f04e7732bc77730304e48f97fdd062be2b142ae34c518ab9b9d7a3b32ec/PKG_HASH:=c755c0aaeec3999cb259a312f3327db205a834abf0beeb6410dcdc818d9719a4/' feeds/packages/net/frp/Makefile
+sed -i 's/PKG_VERSION:=0.53.2/PKG_VERSION:=0.65.0/' feeds/packages/net/frp/Makefile
+sed -i 's/PKG_HASH:=ff2a4f04e7732bc77730304e48f97fdd062be2b142ae34c518ab9b9d7a3b32ec/PKG_HASH:=bbec0d1855e66c96e3a79ff97b8c74d9b1b45ec560aa7132550254d48321f7de/' feeds/packages/net/frp/Makefile
 
-# Éı¼¶zerotierµ½¹Ù·½×îĞÂ°æ±¾1.14.2
+# å‡çº§zerotieråˆ°å®˜æ–¹æœ€æ–°ç‰ˆæœ¬1.14.2
 sed -i 's/PKG_VERSION:=1.14.1/PKG_VERSION:=1.14.2/' feeds/packages/net/zerotier/Makefile
 sed -i 's/PKG_HASH:=4f9f40b27c5a78389ed3f3216c850921f6298749e5819e9f2edabb2672ce9ca0/PKG_HASH:=c2f64339fccf5148a7af089b896678d655fbfccac52ddce7714314a59d7bddbb/' feeds/packages/net/zerotier/Makefile
